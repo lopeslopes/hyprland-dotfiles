339371b30f3a7beae68fb697bb8dde1494e4c546
lopeslopes <mateuslmazziero@gmail.com>
2025-01-27 22:43:03 -0300

    switched from hyprland to sway, and other color changes

---
+++ sway/powermenu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+entries="⇠ Logout\n⅏ Suspend\n↻ Reboot\n⊗ Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 245 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+    swaymsg exit;;
+  suspend)
+    systemctl suspend;;
+  reboot)
+    systemctl reboot;;
+  shutdown)
+    systemctl poweroff -i;;
+esac
